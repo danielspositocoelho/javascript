22cdec45008ee9857b09aa072223d3764c889ddf
Daniel Spósito Coelho <85141838+danielspositocoelho@users.noreply.github.com>
2021-12-08 20:55:51 -0300

    ex1 terminado
    ainda não assisti a resolução

--- CursoYoutube/Exercicios 3-4/3/script.js
+++ CursoYoutube/Exercicios 3-4/3/script.js
@@ -1,4 +1,14 @@
+
+    var resultadoA = document.getElementById("resPa");
+    var resultadoB = document.getElementById("resPg");
+    var emojiSeta = String.fromCodePoint(0x1F449)
+    var emojiFim = String.fromCodePoint(0x1F3F4)
+
 function pa(){
+    if(resultadoA.innerHTML!=""){
+        resultadoA.innerHTML="";
+    }
+
     var ini = document.getElementById("inicio");
     var inicio = Number(ini.value);
 
@@ -7,43 +17,79 @@ function pa(){
 
     var pass = document.getElementById("passo");
     var passo = Number(pass.value)
-
-    var resultado = window.document.getElementById("resPa");
     
+    if(inicio==null||pass==null||fim==null||passo==0||fim==0){
+        window.alert("Verifique os dados e tente novamente - Preencha todos os campos\nA quantidade de termos e a razão não podem ser 0");
+        limparPa();
+    }else{
+        
+    resultadoA.style.border="ridge";
+    resultadoA.style.borderWidth="2px";
+    resultadoA.style.borderColor="aquamarine";  
+    resultadoA.style.height+="200px";
     for(var i=1; i<=fim; i++) // a atualização da variavel deve ser de algum tipo de incremento, auto referencia *= += ++ -- etc...
     {
         if(i!=fim){
-            resultado.innerHTML+=`${inicio.toString()} -->  `;
+            resultadoA.innerHTML+=`${inicio.toString()} ${emojiSeta}  `;
             inicio+=passo;
         } else {     
-            resultado.innerHTML+=`${inicio.toString()} - FIM`;
+            resultadoA.innerHTML+=`${inicio.toString()} - ${emojiFim}`;
             inicio+=passo;
         }
     }
-    
+    }
 }
 
 function pg(){
-    var ini = document.getElementById("inicio");
+    if(resultadoB.innerHTML!=""){
+          resultadoB.innerHTML="";
+    }
+
+    var ini = document.getElementById("inicioB");
     var inicio = Number(ini.value);
 
-    var fi = document.getElementById("fim");
+    var fi = document.getElementById("fimB");
     var fim = Number(fi.value);
 
-    var pass = document.getElementById("passo");
+    var pass = document.getElementById("passoB");
     var passo = Number(pass.value)
 
-    var resultado = window.document.getElementById("resPg");
-    
+    if(inicio==null||pass==null||fim==null||passo==0||passo==1||fim==0){
+        window.alert("Verifique os dados e tente novamente - Preencha todos os campos\nA quantidade de termos não pode ser 0\nA razão não pode ser 0 nem 1");
+        limparPg();
+    }else{
+
+    resultadoB.style.border="ridge";
+    resultadoB.style.borderWidth="2px";
+    resultadoB.style.borderColor="aquamarine";  
+    resultadoB.style.height+="200px";
     for(var i=1; i<=fim; i++) // a atualização da variavel deve ser de algum tipo de incremento, auto referencia *= += ++ -- etc...
     {
         if(i!=fim){
-            resultado.innerHTML+=`${inicio.toString()} -->  `;
+            resultadoB.innerHTML+=`${inicio.toString()} ${emojiSeta}  `;
             inicio*=passo;
         } else {     
-            resultado.innerHTML+=`${inicio.toString()} - FIM`;
+            resultadoB.innerHTML+=`${inicio.toString()} - ${emojiFim}`;
             inicio*=passo;
         }
     }
+    }
+}
+
+function limparPa(){
+    var form = window.document.getElementById("formPa");
+    form.reset();
+    resultadoA.innerHTML="";
+    resultadoA.style-="-200px";
+    resultadoA.style.border="none";
+}
+
+
+function limparPg(){
+    var form = window.document.getElementById("formPg");
+    form.reset();
+    resultadoB.innerHTML="";
+    resultadoB.style-="-200px";
+    resultadoB.style.border="none";
 }
 
