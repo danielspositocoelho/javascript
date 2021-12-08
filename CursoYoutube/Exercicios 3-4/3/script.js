a19af2986c00af8adf649c710503d23e88e815ee
Daniel Spósito Coelho <85141838+danielspositocoelho@users.noreply.github.com>
2021-12-08 14:32:33 -0300

    ex3 v2.0

--- CursoYoutube/Exercicios 3-4/3/script.js
+++ CursoYoutube/Exercicios 3-4/3/script.js
@@ -1,12 +1,37 @@
-function contar(){
-    var inicio = document.getElementById("inicio");
-    var fim = document.getElementById("fim");
-    var passo = document.getElementById("passo");
-    var resultado = document.getElementById("res");
+function pa(){
+    var ini = document.getElementById("inicio");
+    var inicio = Number(ini.value);
+
+    var fi = document.getElementById("fim");
+    var fim = Number(fi.value);
+
+    var pass = document.getElementById("passo");
+    var passo = Number(pass.value)
+
+    var resultado = window.document.getElementById("resPa");
     
-    for(vari=inicio.value; i<=fim.value; i*passo.value)
+    for(var i=inicio; i<fim; i+=passo) // a atualização da variavel deve ser de algum tipo de incremento, auto referencia *= += ++ -- etc...
     {
-        resultado.innerHTML=+`${i} --> `;
+        resultado.innerText+=`${i.toString()} --> `;
+    }
+    
+}
+
+function pg(){
+    var ini = document.getElementById("inicio");
+    var inicio = Number(ini.value);
+
+    var fi = document.getElementById("fim");
+    var fim = Number(fi.value);
+
+    var pass = document.getElementById("passo");
+    var passo = Number(pass.value)
+
+    var resultado = window.document.getElementById("resPg");
+    
+    for(var i=inicio; i<fim; i*=passo) // a atualização da variavel deve ser de algum tipo de incremento, auto referencia *= += ++ -- etc...
+    {
+        resultado.innerText+=`${i.toString()} --> `;
     }
     
 }
